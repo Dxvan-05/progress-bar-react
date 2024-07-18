5188a173dda80e11c68fdcce9aad183068c0d039
Dxvan-05 <118600584+Dxvan-05@users.noreply.github.com>
2024-07-18 20:27:40 +0530

    Fix base 10 to 16 convertion

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,16 @@ import { useContext } from "react";
 import { ProgressContext } from "../App";
 
 const Button = (props) => {
-    const { progress, setProgress } = useContext(ProgressContext);
+    const { progress, setProgress, steps } = useContext(ProgressContext);
 
     const handleButtonClick = () => {
-        if(props.stepBy < 0) {
-            setProgress(Math.max(progress + props.stepBy, 0));
-        }
-        else {
-            setProgress(Math.min(progress + props.stepBy, 100));
+        if(steps) {
+            if(props.stepBy < 0) {
+                setProgress(Math.max(progress + props.stepBy, 0));
+            }
+            else {
+                setProgress(Math.min(progress + props.stepBy, 100));
+            }
         }
     }
     return (
